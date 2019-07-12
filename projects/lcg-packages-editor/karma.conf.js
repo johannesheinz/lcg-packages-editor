12ed914d625a4512b344f9456f41a3da0fa5cb03
Johannes Martin HEINZ <johannes.martin.heinz@cern.ch>
2019-07-12 10:59:08 +0200

    fix: rename project folder also oin config files

--- projects/lcg-packages-editor/karma.conf.js
+++ projects/lcg-packages-editor/karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
     coverageIstanbulReporter: {
       dir: require('path').join(
         __dirname,
-        '../../coverage/angular-ngrx-material-starter'
+        '../../coverage/lcg-packages-editor'
       ),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true

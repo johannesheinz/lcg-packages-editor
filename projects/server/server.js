d96c24c982b53e4862861988d121f250bde5931b
Johannes Martin HEINZ <johannes.martin.heinz@cern.ch>
2019-07-15 10:23:46 +0200

    refactor: use new way to write require: import

--- projects/server/server.js
+++ projects/server/server.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const express = require('express');
-const compression = require('compression');
+import { resolve } from 'path';
+import express, { static } from 'express';
+import compression from 'compression';
 
 const CONTEXT = `/${process.env.CONTEXT || 'lcg-packages-editor'}`;
 const PORT = process.env.PORT || 4000;
@@ -8,14 +8,8 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(compression());
-app.use(
-  CONTEXT,
-  express.static(path.resolve(__dirname, '../../dist/lcg-packages-editor'))
-);
-app.use(
-  '/',
-  express.static(path.resolve(__dirname, '../../dist/lcg-packages-editor'))
-);
+app.use(CONTEXT, static(resolve(__dirname, '../../dist/lcg-packages-editor')));
+app.use('/', static(resolve(__dirname, '../../dist/lcg-packages-editor')));
 app.listen(PORT, () =>
   console.log(`App running on http://localhost:${PORT}${CONTEXT}`)
 );
